4792bbd415a8363620e19d0f78f97c68a2ff9ac7
Abir0248 <abir15-6789@diu.edu.bd>
2023-08-29 22:11:37 +0600

    timer / session added

--- Pages/set-cookie.js
+++ Pages/set-cookie.js
@@ -1,18 +1,29 @@
-import { useEffect } from "react";
-import Cookies from "cookies";
-
 export default function SetCookie() {
-  const handleClick = () => {
+  // expires in 30 minutes
+  const handleClickWithExpiry = () => {
+    const date = new Date();
+    date.setTime(date.getTime() + 30 * 60 * 1000); // 30 minutes
+    document.cookie = `theme=dark; expires=${date.toUTCString()}; path=/;`;
+  };
+
+  // expires when the browser is closed
+  const handleClickWithSession = () => {
     document.cookie = "theme=dark; path=/;";
   };
 
   return (
     <div className="p-4">
       <button
-        className="bg-blue-500 text-white px-4 py-2"
-        onClick={handleClick}
+        className="bg-blue-500 text-white px-4 py-2 mr-4"
+        onClick={handleClickWithExpiry}
       >
-        Set Dark Theme
+        Set Dark Theme (30 mins)
+      </button>
+      <button
+        className="bg-green-500 text-white px-4 py-2"
+        onClick={handleClickWithSession}
+      >
+        Set Dark Theme (Session)
       </button>
     </div>
   );
